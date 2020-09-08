8d3cf6ffc8100d93ae3ab3e00838328d6e2971c2
Gabrielap12 <47859449+Gabrielap12@users.noreply.github.com>
2020-09-08 20:42:33 -0300

    Validacao nome
    Alteracao na validacao do campo nome

--- index.js
+++ index.js
@@ -17,10 +17,9 @@ function mascaraNConta(NContaListener){
 }
 
 function validaTudo(){
-    var caracterNome = /^([0-9_\-\.@])$/;
 
     if(cpf.length > 14 || nome.value == "" || 
-    caracterNome.test(nome.value) == false || tipoConta === 'corrente' && renda > 300 || 
+        tipoConta === 'corrente' && renda > 300 || 
     tipoConta === 'poupanca' && renda > 300 ||  tipoConta === 'especial' && renda > 20900 ||
     tipoConta === 'corrente' && renda < 20900 || tipoConta === 'poupanca' && renda < 20900 ||
     numero.length > 0 || numero.length < 6 || numero.value == "" || cpf.value == "" || renda.value == "")
@@ -40,7 +39,7 @@ function validaTudo(){
         }
 
         //Validacao Nome
-        if(nome.value == "" || caracterNome.test(nome.value) == false){
+        if(nome.value == "" || nome.length < 3){
             return "Nome invalido"
         }
 
